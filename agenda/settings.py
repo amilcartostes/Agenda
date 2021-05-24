72bddaacd5facb513d25b48bbe4b71a9268020cd
amilcartostes <amilcar@quatro.srv.br>
2021-05-24 15:15:24 -0300

    Inclusão dos arquivos de administração do site!

--- agenda/settings.py
+++ agenda/settings.py
@@ -133,10 +133,10 @@ USE_L10N = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'templates/static'),
 ]
 
-# STATIC_ROOT = os.path.join('static')
+STATIC_ROOT = os.path.join('static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
